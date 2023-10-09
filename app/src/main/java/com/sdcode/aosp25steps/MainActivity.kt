a91d64db508a487e362ad54281e3459c237e58a5
SSdCode <dubeysamir514@gmail.com>
2023-10-09 17:34:33 +0530

    Initial commit

---
+++ app/src/main/java/com/sdcode/aosp25steps/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sdcode.aosp25steps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
